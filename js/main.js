7c863be0d8244d08662e581ec82e6a5f64b2690f
BagerManBG <toshko_1998@abv.bg>
2018-06-01 22:50:40 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-window.onload = () => {
-	$('.nav-search').hide();
+const searchBar = () => {
+	$('.nav-search').css({'display': 'none'});
 
-	$(window).scroll(function() {
-		const hT = $('.navbar').offset().top;
-		const wS = $(this).scrollTop();
+	const searchHeight = $('.navbar').offset().top;
+	const windowHeight = $(this).scrollTop();
 
-		if (wS >= hT){
+	if (windowHeight >= searchHeight) {
+		$('.nav-search').fadeIn(200, () => {
+			$('.nav-search').css({'display': 'flex'});
+		});
+	} 
+
+	$(window).scroll(() => {
+		const searchHeight = $('.navbar').offset().top;
+		const windowHeight = $(this).scrollTop();
+
+		if (windowHeight >= searchHeight) {
 			$('.nav-search').fadeIn(200, () => {
 				$('.nav-search').css({'display': 'flex'});
 			});
@@ -13,4 +22,37 @@ window.onload = () => {
 			$('.nav-search').fadeOut(200);
 		}
 	});
+}
+
+const scrollUp = () => {
+	$('.up-arrow').css({'display': 'none'});
+
+	const windowHeight = $(this).scrollTop();
+
+	if (windowHeight > 0) {
+		$('.up-arrow').fadeIn(200, () => {
+			$('.up-arrow').css({'display': 'flex'});
+		});
+	} 
+
+	$(window).scroll(() => {
+		const windowHeight = $(this).scrollTop();
+
+		if (windowHeight > 0) {
+			$('.up-arrow').fadeIn(200, () => {
+				$('.up-arrow').css({'display': 'flex'});
+			});
+		} else {
+			$('.up-arrow').fadeOut(200);
+		}
+	});
+
+	$('.up-arrow').click(() => {
+		$('html, body').animate({ scrollTop: 0 }, 300);
+	});
+}
+
+window.onload = () => {
+	searchBar();
+	scrollUp();
 }
